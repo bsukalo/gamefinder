d36675f7ba48af30f29e439eb0d735c7da80bf4e
bsukalo <bsukalo@outlook.com>
2023-10-06 23:53:21 +0200

    Show skeleton while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,41 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import {
+	HStack,
+	List,
+	ListItem,
+	Image,
+	Text,
+	Skeleton,
+	SkeletonText,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-	const { data } = useGenres();
+	const { data, isLoading } = useGenres();
+	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+	if (isLoading) {
+		return (
+			<List>
+				{skeletons.map((skeleton) => {
+					return (
+						<ListItem key={skeleton} paddingY="5px">
+							<HStack>
+								<Skeleton>
+									<Image
+										boxSize="32px"
+										borderRadius={8}
+									></Image>
+								</Skeleton>
+								<SkeletonText noOfLines={1} width="120px" />
+							</HStack>
+						</ListItem>
+					);
+				})}
+			</List>
+		);
+	}
+
 	return (
 		<List>
 			{data.map((genre) => {
