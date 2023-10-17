f56acb1ffe80524a4d82d83e2cc4998fa14dbb95
bsukalo <bsukalo@outlook.com>
2023-10-17 20:32:16 +0200

    Build sort selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -14,7 +14,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
 	if (error) return null;
 	return (
 		<Menu>
-			<MenuButton as={Button} rightIcon={<BsChevronDown />} ml="10px">
+			<MenuButton as={Button} rightIcon={<BsChevronDown />}>
 				{selectedPlatform?.name || "Platforms"}
 			</MenuButton>
 			<MenuList>
