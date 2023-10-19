0321908000d16acb45c7fe3fd98dd80b929979d4
bsukalo <bsukalo@outlook.com>
2023-10-19 20:46:10 +0200

    Update logo in light mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/gamefinder.webp";
+import { HStack, Image, useColorModeValue } from "@chakra-ui/react";
+import lightLogo from "../assets/logo-light.webp";
+import darkLogo from "../assets/logo-dark.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const Navbar = () => {
+	const theme = useColorModeValue("light", "dark");
+
 	return (
 		<HStack justifyContent={"space-between"} padding={"10px"}>
-			<Image src={logo} maxWidth={"180px"} />
+			{theme === "light" ? (
+				<Image src={lightLogo} maxWidth={"180px"} />
+			) : (
+				<Image src={darkLogo} maxWidth={"180px"} />
+			)}
+
 			<SearchInput />
 			<ColorModeSwitch />
 		</HStack>
