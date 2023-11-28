0dcdff91d8f7e1df58f2de667ea827a425afb63a
bsukalo <bsukalo@outlook.com>
2023-11-28 22:04:00 +0100

    Tweak color mode switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,11 +1,19 @@
 import { Button, useColorMode } from "@chakra-ui/react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 const ColorModeSwitch = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
 
 	return (
-		<Button onClick={toggleColorMode} size="sm" minWidth={"80px"}>
-			{colorMode === "light" ? "Dark" : "Light"} mode
+		<Button
+			onClick={toggleColorMode}
+			size={"40px"}
+			minWidth={"40px"}
+			minHeight={"40px"}
+			borderRadius={10}
+			fontSize={"1.3em"}
+		>
+			{colorMode === "light" ? <MdDarkMode /> : <MdLightMode />}
 		</Button>
 	);
 };
